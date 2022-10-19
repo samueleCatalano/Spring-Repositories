00d397ab1977b738feb6b43f335b2bc6e4d1f5c1
Samuele Catalano <csamuele73@mail.com>
2022-10-19 18:05:32 +0200

    first commit

---
+++ repositories/src/test/java/com/repositories/repositories/RepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repositories.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
